2fb1d3520a8c3e82eb82ca12800896af88d757c0
Chloe Deng <Chloe.deng1000@gmail.com>
2020-07-10 16:57:19 -0700

    Initial Commit

---
+++ QuizApp2.0/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QuizApp2.0
+//
+//  Created by Apple on 7/10/20.
+//  Copyright © 2020 Chloe Deng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
